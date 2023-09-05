b5b8b61a24f388bc27f8f63aaa330459afcd1d4d
jampen <pendergastj@proton.me>
2023-09-05 11:31:21 +0100

    user input

--- src/fileorg.py
+++ src/fileorg.py
@@ -20,17 +20,27 @@ def apply(fsmap):
         for category, files in categories.items():
             path_to_category = f'{basedir}/{category}'
 
-            if os.path.exists(path_to_category):
-                os.rmdir(path_to_category)
+            # Create the subdirectory if it does not already exist
+            if not os.path.isdir(path_to_category):
+                os.mkdir(path_to_category)
 
-            os.mkdir(path_to_category)
-
-            # Move the files
-
+            # Move the organized files into the categorized subdiretories:
             for file in files:
+                # Source path:
                 base_path_to_file = f'{basedir}/{file}'
+                # Destination path:
                 moved_file_path = f'{path_to_category}/{file}'
-                shutil.move(base_path_to_file, moved_file_path)
+
+                # If there is already a file at the destination, then
+                # the user has most likely ran this script twice
+                if os.path.isfile(moved_file_path):
+                    continue
+
+                # Move the file from its original position to be inside the category
+                if os.path.isfile(base_path_to_file):
+                    shutil.move(base_path_to_file, moved_file_path)
+                else:
+                    print(f'{base_path_to_file} does not exist.')
 
 
 apply(fsmap=read_file())
